8bd8797111149f83686858cd9e081a3cd7c46c10
Kristian Larsson <kristian@tutamail.com>
2019-01-02 19:25:21 +0100

    finally working

--- app.js
+++ app.js
@@ -5,23 +5,26 @@ const request = require('request');
 
 const {API_KEY, API_SECRET, CLOUD_NAME} = require('./cloudinaryApiKeys');
 console.log("If these are displayed as ****, update file cloudinaryApiKeys.js.", API_KEY, API_SECRET, CLOUD_NAME);
-const base_url = 'https://' + API_KEY + ':' + API_SECRET + '@api.cloudinary.com/v1_1/' + CLOUD_NAME + '/resources/image?max_results=5';
+const base_url = 'https://' + API_KEY + ':' + API_SECRET + '@api.cloudinary.com/v1_1/' + CLOUD_NAME + '/resources/image?max_results=500';
 
-const downloadImage = (img) => {
-  const {url, secure_url, public_id, format} = img;
-  const filename = public_id.replace(/\//g, '_') + '.' + format;
-  request.head(url, function(err, res, body){
+const downloadImage = (url, filename, callback) => {
+  request.head(url, function(err, res, body) {
     request(url).pipe(
       fs.createWriteStream('./downloads/' + filename)
-    ).on('close', () => console.log('downloaded', filename));
+    ).on('close', callback);
   });
-};
+}
 
 const downloadImages = (list) => {
-  console.log('Total number of images ids in list:', list.length, list);
-  for (const img of list) {
-    downloadImage(img)
+  if (list.length <= 0) {
+    console.log('done!')
+    return
   }
+  console.log('remaining images:', list.length);
+  const img = list.pop();
+  const {url, secure_url, public_id, format} = img;
+  const filename = public_id.replace(/\//g, '_') + '.' + format;
+  downloadImage(url, filename, () => downloadImages(list));
 }
 
 const getImageList = (url) => {
@@ -49,7 +52,7 @@ const getImageLists = async (url, list = []) => {
   const data = await getImageList(url);
   const {resources, next_cursor = null} = JSON.parse(data);
   list = [...list, ...resources]
-  if (!next_cursor) {
+  if (next_cursor) {
     console.log('fetched', list.length, 'image ids');
     const next_url = base_url + '&next_cursor=' + next_cursor;
     getImageLists(next_url, list);
