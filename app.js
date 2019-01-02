7c737f28c20ab236a480f2215badb5f1f4e3fe97
Kristian Larsson <kristian@tutamail.com>
2019-01-02 18:03:13 +0100

    working but funky, going a bit random

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const request = require('request');
 
 const {API_KEY, API_SECRET, CLOUD_NAME} = require('./cloudinaryApiKeys');
 console.log("If these are displayed as ****, update file cloudinaryApiKeys.js.", API_KEY, API_SECRET, CLOUD_NAME);
-const base_url = 'https://' + API_KEY + ':' + API_SECRET + '@api.cloudinary.com/v1_1/' + CLOUD_NAME + '/resources/image?max_results=500';
+const base_url = 'https://' + API_KEY + ':' + API_SECRET + '@api.cloudinary.com/v1_1/' + CLOUD_NAME + '/resources/image?max_results=10';
 
-const downloadImage = (uri, filename) => {
+const downloadImage = async (uri, filename) => {
   request.head(uri, function(err, res, body){
     request(uri).pipe(
       fs.createWriteStream('./downloads/' + filename)
@@ -15,13 +15,19 @@ const downloadImage = (uri, filename) => {
   });
 };
 
-const downloadImages = (resources) => {
-  console.log('number of images in list:', resources.length);
+const iterateImages = async (resources) => {
   for (const img of resources) {
     const {url, secure_url, public_id, format} = img;
     const filename = public_id.replace(/\//g, '_') + '.' + format;
-    downloadImage(url, filename);
+    await downloadImage(url, filename);
   }
+};
+
+const downloadImages = async (data) => {
+  const {resources, next_cursor = null} = JSON.parse(data);
+  console.log('number of images in list:', resources.length);
+  const iterated = await iterateImages(resources);
+  console.log('iterated returned:', iterated);
 }
 
 const getImageList = (url) => {
@@ -47,8 +53,8 @@ const getImageList = (url) => {
 
 const getImageLists = async (url) => {
   const data = await getImageList(url);
-  const {resources, next_cursor = null} = JSON.parse(data);
-  downloadImages(resources)
+
+  const next_cursor = await downloadImages(data)
   if (next_cursor) {
     const next_url = base_url + '&next_cursor=' + next_cursor;
     getImageLists(next_url);
