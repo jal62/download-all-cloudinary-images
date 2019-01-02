55d7d7d18bf32ab1144dda95113d05d7d1575c87
Kristian Larsson <kristian@tutamail.com>
2019-01-02 18:18:02 +0100

    downloading all lists first

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const request = require('request');
 
 const {API_KEY, API_SECRET, CLOUD_NAME} = require('./cloudinaryApiKeys');
 console.log("If these are displayed as ****, update file cloudinaryApiKeys.js.", API_KEY, API_SECRET, CLOUD_NAME);
-const base_url = 'https://' + API_KEY + ':' + API_SECRET + '@api.cloudinary.com/v1_1/' + CLOUD_NAME + '/resources/image?max_results=10';
+const base_url = 'https://' + API_KEY + ':' + API_SECRET + '@api.cloudinary.com/v1_1/' + CLOUD_NAME + '/resources/image?max_results=500';
 
-const downloadImage = async (uri, filename) => {
+const downloadImage = (uri, filename) => {
   request.head(uri, function(err, res, body){
     request(uri).pipe(
       fs.createWriteStream('./downloads/' + filename)
@@ -15,19 +15,13 @@ const downloadImage = async (uri, filename) => {
   });
 };
 
-const iterateImages = async (resources) => {
-  for (const img of resources) {
+const downloadImages = async (list) => {
+  console.log('number of images in list:', list.length);
+  for (const img of list) {
     const {url, secure_url, public_id, format} = img;
     const filename = public_id.replace(/\//g, '_') + '.' + format;
     await downloadImage(url, filename);
   }
-};
-
-const downloadImages = async (data) => {
-  const {resources, next_cursor = null} = JSON.parse(data);
-  console.log('number of images in list:', resources.length);
-  const iterated = await iterateImages(resources);
-  console.log('iterated returned:', iterated);
 }
 
 const getImageList = (url) => {
@@ -51,13 +45,18 @@ const getImageList = (url) => {
   });
 };
 
-const getImageLists = async (url) => {
+const getImageLists = async (url, list = []) => {
+  console.log('in getImageLists', list.length)
   const data = await getImageList(url);
-
-  const next_cursor = await downloadImages(data)
+  const {resources, next_cursor = null} = JSON.parse(data);
+  list = [...resources, ...list]
   if (next_cursor) {
+    console.log('in getImageLists, if next_cursor', next_cursor, list.length)
     const next_url = base_url + '&next_cursor=' + next_cursor;
-    getImageLists(next_url);
+    getImageLists(next_url, list);
+  } else {
+    console.log('in getImageLists, if not next_cursor', next_cursor, list.length)
+    downloadImages(list)
   }
 }
 
