f68fc1c6f4cf4a00f8212e50e01c2719fb62f74d
jal62 <31416144+jal62@users.noreply.github.com>
2019-01-10 13:15:36 -0800

    Update cloudinaryApiKeys.js

--- cloudinaryApiKeys.js
+++ cloudinaryApiKeys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_KEY: "***************",
-  API_SECRET: "***************************",
-  CLOUD_NAME: "*********"
+  API_KEY: "524425278773938",
+  API_SECRET: "6UxqE1nuMEKBC03QVoHkoh1ifBw",
+  CLOUD_NAME: "reso"
 };
